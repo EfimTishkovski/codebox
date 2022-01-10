0c058044dbae98e3a68df852f4237943472a7f3b
Efim <efimpost@gmail.com>
2022-01-10 14:32:24 +0300

    Подправлены батники, баги с русскими буквами при смене папки.

--- install.bat
+++ install.bat
@@ -1,7 +1,5 @@
-@ echo off
-chcp 65001
-rem Создание виртуальной среды
-start/b "" python -m venv venv 
-echo Виртуальна среда успешно создана.
-echo Готово.Закройте командную строку. Запустите install_step2.bat
+@echo off
+start/b "" python -m venv venv
+echo venv be done.
+echo Ready. Launch install_step2.bat
 pause & exit
--- install_step2.bat
+++ install_step2.bat
@@ -1,11 +1,8 @@
 @echo off
-chcp 65001
-echo Активация виртуальной среды
-start/b "" venv\Scripts\activate
+start/b  ""  venv\Scripts\activate
 venv\Scripts\pip install -r requirements.txt
-echo Пакеты успешно установлены
-echo Готово. Закройте командную строку.
-echo Запуск приложения: запустите файл launch.bat
+echo Done. Closed commandline.
+echo launch:  launch.bat.
 exit /b
 
 
